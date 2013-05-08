6af6835da1ff046e25b7384358591ad99b37467e
eduard@valeyev.net <eduard@valeyev.net>
2013-05-08 16:47:02 -0400

    1) Updated FSF address and refreshed the contents of the LICENSE file. 2) Added CITATION file 3) Renamed configure.in to configure.ac 4) Bumped libtool to 2.4.2 and config.guess/sub to the latest 5) Refreshed INSTALL file 6) Added COPYING file with GPLv2
    RESOLVED - issue LIBINT-38: License info is out of date.
    http://valeev.chem.vt.edu:8080/browse/LIBINT-38
    
    --HG--
    rename : configure.in => configure.ac

--- bin/update_libtool.sh
+++ bin/update_libtool.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-glibtoolize --copy --force
+glibtoolize -v --copy --force
 
 curl 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' > config.guess
 curl 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' > config.sub
