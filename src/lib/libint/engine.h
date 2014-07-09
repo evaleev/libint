evaleev/libint

commit 8cc13f70fb4de33066c80156d407a058bfc0ec5d

HRR-less build of kinetic energy ints implemented.

src/lib/libint/engine.h
@@ -35,8 +35,9 @@ namespace libint2 {
 
         const auto ncart_max = (lmax_+1)*(lmax_+2)/2;
 
+        assert(deriv_order_ <= LIBINT2_DERIV_ONEBODY_ORDER);
+
         if (type_ == overlap) {
-          assert(deriv_order_ <= LIBINT2_DERIV_ONEBODY_ORDER);
           switch(deriv_order_) {
 
             case 0:
@@ -61,7 +62,32 @@ namespace libint2 {
           return;
         }
 
-        assert(type_ == overlap);
+        if (type_ == kinetic) {
+          switch(deriv_order_) {
+
+            case 0:
+              libint2_init_kinetic(&primdata_[0], lmax_, 0);
+              scratch_.resize(ncart_max*ncart_max);
+              break;
+            case 1:
+#if LIBINT2_DERIV_ONEBODY_ORDER > 0
+              libint2_init_kinetic1(&primdata_[0], lmax_, 0);
+              scratch_.resize(3 * ncart_max*ncart_max);
+#endif
+              break;
+            case 2:
+#if LIBINT2_DERIV_ONEBODY_ORDER > 1
+              libint2_init_kinetic2(&primdata_[0], lmax_, 0);
+              scratch_.resize(6 * ncart_max*ncart_max);
+#endif
+              break;
+            default: assert(deriv_order_ < 3);
+          }
+
+          return;
+        }
+
+        assert(type_ == overlap || type_ == kinetic);
       }
 
       /// computes shell set of integrals
@@ -107,12 +133,31 @@ namespace libint2 {
         if (lmax == 0) { // (s|s)
           auto& result = primdata_[0].stack[0];
           result = 0;
-          for(auto p12=0; p12 != nprimpairs; ++p12)
-            result += primdata_[p12]._aB_s___0___Overlap_s___0___Ab__up_[0];
+          switch (type_) {
+            case overlap:
+              for(auto p12=0; p12 != nprimpairs; ++p12)
+                result += primdata_[p12].LIBINT_T_S_OVERLAP_S[0];
+              break;
+            case kinetic:
+              for(auto p12=0; p12 != nprimpairs; ++p12)
+                result += primdata_[p12].LIBINT_T_S_KINETIC_S[0];
+              break;
+            default:
+              assert(false);
+          }
           primdata_[0].targets[0] = primdata_[0].stack;
         }
         else {
-          LIBINT2_PREFIXED_NAME(libint2_build_overlap)[bra.contr[0].l][ket.contr[0].l](&primdata_[0]);
+          switch (type_) {
+            case overlap:
+              LIBINT2_PREFIXED_NAME(libint2_build_overlap)[bra.contr[0].l][ket.contr[0].l](&primdata_[0]);
+              break;
+            case kinetic:
+              LIBINT2_PREFIXED_NAME(libint2_build_kinetic)[bra.contr[0].l][ket.contr[0].l](&primdata_[0]);
+              break;
+            default:
+              assert(false);
+          }
         }
 
         if (swap) {
@@ -161,7 +206,9 @@ namespace libint2 {
         const double PBy = Py - B[1];
         const double PBz = Pz - B[2];
 
-#if LIBINT2_SHELLQUARTET_SET == LIBINT2_SHELLQUARTET_SET_STANDARD // always VRR on bra, and HRR to bra
+        if (LIBINT2_SHELLQUARTET_SET == LIBINT2_SHELLQUARTET_SET_STANDARD // always VRR on bra, and HRR to bra (overlap, coulomb)
+            || type_ == kinetic // kinetic energy ints don't use HRR, hence VRR on both centers
+           ) {
 
 #if LIBINT2_DEFINED(eri,PA_x)
         primdata.PA_x[0] = Px - A[0];
@@ -181,8 +228,8 @@ namespace libint2 {
 #if LIBINT2_DEFINED(eri,AB_z)
         primdata.AB_z[0] = A[2] - B[2];
 #endif
-
-#else // always VRR on ket, HRR to ket
+        }
+        else { // always VRR on ket, HRR to ket (overlap, coulomb), or no HRR at all (kinetic energy ints)
 
 #if LIBINT2_DEFINED(eri,PB_x)
         primdata.PB_x[0] = Px - B[0];
@@ -202,12 +249,23 @@ namespace libint2 {
 #if LIBINT2_DEFINED(eri,BA_z)
         primdata.BA_z[0] = B[2] - A[2];
 #endif
-
-#endif
+        }
 
 #if LIBINT2_DEFINED(eri,oo2z)
         primdata.oo2z[0] = 0.5*oogammap;
 #endif
+
+        if (type_ == kinetic) { // additional factors for kinetic energy
+#if LIBINT2_DEFINED(eri,rho12_over_alpha1)
+        primdata.rho12_over_alpha1[0] = alpha2 * oogammap;
+#endif
+#if LIBINT2_DEFINED(eri,rho12_over_alpha2)
+        primdata.rho12_over_alpha2[0] = alpha1 * oogammap;
+#endif
+#if LIBINT2_DEFINED(eri,two_rho12)
+        primdata.two_rho12[0] = 2. * rhop;
+#endif
+        }
 
         if (deriv_order_ > 0) {
           // prefactors for derivative overlap relations
@@ -219,6 +277,9 @@ namespace libint2 {
         const auto ovlp_ss = sqrt_PI_cubed * sqrt(oogammap) * K1 * c1 * c2;
 
         primdata.LIBINT_T_S_OVERLAP_S[0] = ovlp_ss;
+        if (type_ == kinetic) {
+          primdata.LIBINT_T_S_KINETIC_S[0] = rhop * (3. - 2.*rhop*AB2) * ovlp_ss;
+        }
 
       }
 

include/libint2.h
@@ -8,6 +8,7 @@
 #define LIBINT_T_SS_K2G12_SS_0 _aB_s___0__s___1___r12_2_g12_s___0__s___1___Ab__up_0
 #define LIBINT_T_SS_K4G12_SS_0 _aB_s___0__s___1___r12_4_g12_s___0__s___1___Ab__up_0
 #define LIBINT_T_S_OVERLAP_S _aB_s___0___Overlap_s___0___Ab__up_
+#define LIBINT_T_S_KINETIC_S _aB_s___0___Kinetic_s___0___Ab__up_
 
 #include <libint2_intrinsic_types.h>
 #include <libint2_params.h>

src/bin/libint/build_libint.cc
@@ -137,14 +137,6 @@ static void print_config(std::ostream& os);
 // Put all configuration-specific API elements in here
 static void config_to_api(const SafePtr<CompilationParameters>& cparams, SafePtr<Libint2Iface>& iface);
 
-#ifdef INCLUDE_ONEBODY
-# if  LIBINT_SUPPORT_ONEBODYINTS == 0
-#  error "change LIBINT_SUPPORT_ONEBODYINTS in global_macros.h to 1 if need 1-body ints"
-# endif
-  static void build_Overlap_1b_1k(std::ostream& os, const SafePtr<CompilationParameters>& cparams,
-                                  SafePtr<Libint2Iface>& iface, unsigned int deriv_level);
-#endif
-
 #ifdef INCLUDE_ERI
 #define USE_GENERIC_ERI_BUILD 1
 # if !USE_GENERIC_ERI_BUILD
@@ -176,6 +168,171 @@ static void build_R12kG12_2b_2k_separate(std::ostream& os, const SafePtr<Compila
 #ifdef INCLUDE_G12DKH
 static void build_G12DKH_2b_2k(std::ostream& os, const SafePtr<CompilationParameters>& cparams,
                                 SafePtr<Libint2Iface>& iface);
+#endif
+
+#ifdef INCLUDE_ONEBODY
+
+#  if  LIBINT_SUPPORT_ONEBODYINTS == 0
+#  error "change LIBINT_SUPPORT_ONEBODYINTS in global_macros.h to 1 if need 1-body ints"
+# endif
+
+template <typename ShellSet>
+void
+build_onebody_1b_1k(std::ostream& os, std::string label, const SafePtr<CompilationParameters>& cparams,
+                    SafePtr<Libint2Iface>& iface, unsigned int deriv_level)
+{
+  const std::string task = task_label(label, deriv_level);
+  const std::string task_uc = task_label(label, deriv_level);
+  typedef ShellSet Onebody_sh_1_1;
+  vector<CGShell*> shells;
+  unsigned int lmax = cparams->max_am(task);
+  for(unsigned int l=0; l<=lmax; l++) {
+    shells.push_back(new CGShell(l));
+  }
+  ImplicitDimensions::set_default_dims(cparams);
+
+  LibraryTaskManager& taskmgr = LibraryTaskManager::Instance();
+  taskmgr.current(task);
+  iface->to_params(iface->macro_define( std::string("MAX_AM_") + task_uc,lmax));
+
+  //
+  // Construct graphs for each desired target integral and
+  // 1) generate source code for the found traversal path
+  // 2) extract all remaining unresolved recurrence relations and
+  //    append them to the stack. Such unresolved RRs are RRs applied
+  //    to sets of integrals (rather than to individual integrals).
+  // 3) at the end, for each unresolved recurrence relation generate
+  //    explicit source code
+  //
+  SafePtr<DirectedGraph> dg(new DirectedGraph);
+  SafePtr<Strategy> strat(new Strategy());
+  SafePtr<Tactic> tactic(new FirstChoiceTactic<DummyRandomizePolicy>);
+  SafePtr<CodeContext> context(new CppCodeContext(cparams));
+  SafePtr<MemoryManager> memman(new WorstFitMemoryManager());
+
+  for(unsigned int la=0; la<=lmax; la++) {
+    for(unsigned int lb=0; lb<=lmax; lb++) {
+
+          // skip s|s integrals -- no need to involve LIBINT here
+          if (deriv_level == 0 && la == 0 && lb == 0)
+            continue;
+
+#if STUDY_MEMORY_USAGE
+          const int lim = 1;
+          if (! (la == lim && lb == lim) )
+            continue;
+#endif
+
+          // unroll only if max_am <= cparams->max_am_opt(task)
+          using std::max;
+          const unsigned int max_am = max(la,lb);
+          const bool need_to_optimize = (max_am <= cparams->max_am_opt(task));
+          const unsigned int unroll_threshold = need_to_optimize ? cparams->unroll_threshold() : 0;
+          dg->registry()->unroll_threshold(unroll_threshold);
+          dg->registry()->do_cse(need_to_optimize);
+          dg->registry()->condense_expr(condense_expr(cparams->unroll_threshold(),cparams->max_vector_length()>1));
+          // Need to accumulate integrals?
+          dg->registry()->accumulate_targets(cparams->accumulate_targets());
+
+          ////////////
+          // loop over unique derivative index combinations
+          ////////////
+          // skip 1 center -- all derivatives with respect to that center can be
+          // recovered using translational invariance conditions
+          // which center to skip? -> A = 0, B = 1
+          const unsigned int center_to_skip = 1;
+          DerivIndexIterator<1> diter(deriv_level);
+          std::vector< SafePtr<Onebody_sh_1_1> > targets;
+          bool last_deriv = false;
+          do {
+            CGShell a(la);
+            CGShell b(lb);
+
+            unsigned int center = 0;
+            for(unsigned int i=0; i<2; ++i) {
+              if (i == center_to_skip)
+                continue;
+              for(unsigned int xyz=0; xyz<3; ++xyz) {
+                if (i == 0) a.deriv().inc(xyz, diter.value(3 * center + xyz));
+                if (i == 1) b.deriv().inc(xyz, diter.value(3 * center + xyz));
+              }
+              ++center;
+            }
+
+            SafePtr<Onebody_sh_1_1> target = Onebody_sh_1_1::Instance(a,b);
+            targets.push_back(target);
+            last_deriv = diter.last();
+            if (!last_deriv) diter.next();
+          } while (!last_deriv);
+          // append all derivatives as targets to the graph
+          for(typename std::vector< SafePtr<Onebody_sh_1_1> >::const_iterator t=targets.begin();
+              t != targets.end();
+              ++t) {
+            SafePtr<DGVertex> t_ptr = dynamic_pointer_cast<DGVertex,Onebody_sh_1_1>(*t);
+            dg->append_target(t_ptr);
+          }
+
+          // make label that characterizes this set of targets
+          // use the label of the nondifferentiated integral as a base
+          std::string ab_label;
+          {
+            CGShell a(la);
+            CGShell b(lb);
+            SafePtr<Onebody_sh_1_1> ab = Onebody_sh_1_1::Instance(a,b);
+            ab_label = ab->label();
+          }
+          // + derivative level (if deriv_level > 0)
+          std::string label;
+          {
+            label = cparams->api_prefix();
+            if (deriv_level != 0) {
+              std::ostringstream oss;
+              oss << "deriv" << deriv_level;
+              label += oss.str();
+            }
+            label += ab_label;
+          }
+
+          std::string prefix(cparams->source_directory());
+          std::deque<std::string> decl_filenames;
+          std::deque<std::string> def_filenames;
+
+          // this will generate code for this targets, and potentially generate code for its prerequisites
+          GenerateCode(dg, context, cparams, strat, tactic, memman,
+                       decl_filenames, def_filenames,
+                       prefix, label, false);
+
+          // update max stack size and # of targets
+          const SafePtr<TaskParameters>& tparams = taskmgr.current().params();
+          tparams->max_stack_size(max_am, memman->max_memory_used());
+          tparams->max_ntarget(targets.size());
+          //os << " Max memory used = " << memman->max_memory_used() << std::endl;
+
+          // set pointer to the top-level evaluator function
+          ostringstream oss;
+          oss << context->label_to_name(cparams->api_prefix()) << "libint2_build_" << task << "[" << la << "][" << lb << "] = "
+              << context->label_to_name(label_to_funcname(label))
+              << context->end_of_stat() << endl;
+          iface->to_static_init(oss.str());
+
+          // need to declare this function internally
+          for(std::deque<std::string>::const_iterator i=decl_filenames.begin();
+              i != decl_filenames.end();
+              ++i) {
+            oss.str("");
+            oss << "#include <" << *i << ">" << endl;
+            iface->to_int_iface(oss.str());
+          }
+
+#if DEBUG
+          os << "Max memory used = " << memman->max_memory_used() << endl;
+#endif
+          dg->reset();
+          memman->reset();
+
+    } // end of b loop
+  } // end of a loop
+}
 #endif
 
 void try_main (int argc, char* argv[])
@@ -408,7 +565,8 @@ void try_main (int argc, char* argv[])
 
 #ifdef INCLUDE_ONEBODY
   for(unsigned int d=0; d<=INCLUDE_ONEBODY; ++d) {
-    build_Overlap_1b_1k(os,cparams,iface,d);
+    build_onebody_1b_1k<Overlap_1_1_sq>(os,"overlap",cparams,iface,d);
+    build_onebody_1b_1k<Kinetic_1_1_sq>(os,"kinetic",cparams,iface,d);
   }
 #endif
 #ifdef INCLUDE_ERI
@@ -525,164 +683,6 @@ print_config(std::ostream& os)
 #endif
 }
 
-#ifdef INCLUDE_ONEBODY
-void
-build_Overlap_1b_1k(std::ostream& os, const SafePtr<CompilationParameters>& cparams,
-                    SafePtr<Libint2Iface>& iface, unsigned int deriv_level)
-{
-  const std::string task = task_label("overlap", deriv_level);
-  const std::string task_uc = task_label("overlap", deriv_level);
-  typedef Overlap_1_1_sq Overlap_sh_1_1;
-  vector<CGShell*> shells;
-  unsigned int lmax = cparams->max_am(task);
-  for(unsigned int l=0; l<=lmax; l++) {
-    shells.push_back(new CGShell(l));
-  }
-  ImplicitDimensions::set_default_dims(cparams);
-
-  LibraryTaskManager& taskmgr = LibraryTaskManager::Instance();
-  taskmgr.current(task);
-  iface->to_params(iface->macro_define( std::string("MAX_AM_") + task_uc,lmax));
-
-  //
-  // Construct graphs for each desired target integral and
-  // 1) generate source code for the found traversal path
-  // 2) extract all remaining unresolved recurrence relations and
-  //    append them to the stack. Such unresolved RRs are RRs applied
-  //    to sets of integrals (rather than to individual integrals).
-  // 3) at the end, for each unresolved recurrence relation generate
-  //    explicit source code
-  //
-  SafePtr<DirectedGraph> dg(new DirectedGraph);
-  SafePtr<Strategy> strat(new Strategy());
-  SafePtr<Tactic> tactic(new FirstChoiceTactic<DummyRandomizePolicy>);
-  SafePtr<CodeContext> context(new CppCodeContext(cparams));
-  SafePtr<MemoryManager> memman(new WorstFitMemoryManager());
-
-  for(unsigned int la=0; la<=lmax; la++) {
-    for(unsigned int lb=0; lb<=lmax; lb++) {
-
-          // skip s|s integrals -- no need to involve LIBINT here
-          if (deriv_level == 0 && la == 0 && lb == 0)
-            continue;
-
-#if STUDY_MEMORY_USAGE
-          const int lim = 1;
-          if (! (la == lim && lb == lim) )
-            continue;
-#endif
-
-          // unroll only if max_am <= cparams->max_am_opt(task)
-          using std::max;
-          const unsigned int max_am = max(la,lb);
-          const bool need_to_optimize = (max_am <= cparams->max_am_opt(task));
-          const unsigned int unroll_threshold = need_to_optimize ? cparams->unroll_threshold() : 0;
-          dg->registry()->unroll_threshold(unroll_threshold);
-          dg->registry()->do_cse(need_to_optimize);
-          dg->registry()->condense_expr(condense_expr(cparams->unroll_threshold(),cparams->max_vector_length()>1));
-          // Need to accumulate integrals?
-          dg->registry()->accumulate_targets(cparams->accumulate_targets());
-
-          ////////////
-          // loop over unique derivative index combinations
-          ////////////
-          // skip 1 center -- all derivatives with respect to that center can be
-          // recovered using translational invariance conditions
-          // which center to skip? -> A = 0, B = 1
-          const unsigned int center_to_skip = 1;
-          DerivIndexIterator<1> diter(deriv_level);
-          std::vector< SafePtr<Overlap_sh_1_1> > targets;
-          bool last_deriv = false;
-          do {
-            CGShell a(la);
-            CGShell b(lb);
-
-            unsigned int center = 0;
-            for(unsigned int i=0; i<2; ++i) {
-              if (i == center_to_skip)
-                continue;
-              for(unsigned int xyz=0; xyz<3; ++xyz) {
-                if (i == 0) a.deriv().inc(xyz, diter.value(3 * center + xyz));
-                if (i == 1) b.deriv().inc(xyz, diter.value(3 * center + xyz));
-              }
-              ++center;
-            }
-
-            SafePtr<Overlap_sh_1_1> target = Overlap_sh_1_1::Instance(a,b);
-            targets.push_back(target);
-            last_deriv = diter.last();
-            if (!last_deriv) diter.next();
-          } while (!last_deriv);
-          // append all derivatives as targets to the graph
-          for(std::vector< SafePtr<Overlap_sh_1_1> >::const_iterator t=targets.begin();
-              t != targets.end();
-              ++t) {
-            SafePtr<DGVertex> t_ptr = dynamic_pointer_cast<DGVertex,Overlap_sh_1_1>(*t);
-            dg->append_target(t_ptr);
-          }
-
-          // make label that characterizes this set of targets
-          // use the label of the nondifferentiated integral as a base
-          std::string ab_label;
-          {
-            CGShell a(la);
-            CGShell b(lb);
-            SafePtr<Overlap_sh_1_1> ab = Overlap_sh_1_1::Instance(a,b);
-            ab_label = ab->label();
-          }
-          // + derivative level (if deriv_level > 0)
-          std::string label;
-          {
-            label = cparams->api_prefix();
-            if (deriv_level != 0) {
-              std::ostringstream oss;
-              oss << "deriv" << deriv_level;
-              label += oss.str();
-            }
-            label += ab_label;
-          }
-
-          std::string prefix(cparams->source_directory());
-          std::deque<std::string> decl_filenames;
-          std::deque<std::string> def_filenames;
-
-          // this will generate code for this targets, and potentially generate code for its prerequisites
-          GenerateCode(dg, context, cparams, strat, tactic, memman,
-                       decl_filenames, def_filenames,
-                       prefix, label, false);
-
-          // update max stack size and # of targets
-          const SafePtr<TaskParameters>& tparams = taskmgr.current().params();
-          tparams->max_stack_size(max_am, memman->max_memory_used());
-          tparams->max_ntarget(targets.size());
-          //os << " Max memory used = " << memman->max_memory_used() << std::endl;
-
-          // set pointer to the top-level evaluator function
-          ostringstream oss;
-          oss << context->label_to_name(cparams->api_prefix()) << "libint2_build_" << task << "[" << la << "][" << lb << "] = "
-              << context->label_to_name(label_to_funcname(label))
-              << context->end_of_stat() << endl;
-          iface->to_static_init(oss.str());
-
-          // need to declare this function internally
-          for(std::deque<std::string>::const_iterator i=decl_filenames.begin();
-              i != decl_filenames.end();
-              ++i) {
-            oss.str("");
-            oss << "#include <" << *i << ">" << endl;
-            iface->to_int_iface(oss.str());
-          }
-
-#if DEBUG
-          os << "Max memory used = " << memman->max_memory_used() << endl;
-#endif
-          dg->reset();
-          memman->reset();
-
-    } // end of b loop
-  } // end of a loop
-}
-#endif
 
 #ifdef INCLUDE_ERI
 void

src/bin/libint/master_ints_list.h
@@ -18,6 +18,8 @@ namespace libint2 {
 #if LIBINT_SUPPORT_ONEBODYINTS
   typedef GenIntegralSet_1_1<CGShell,OverlapOper,EmptySet> Overlap_1_1_sq;
   typedef GenIntegralSet_1_1<CGF,OverlapOper,EmptySet> Overlap_1_1_int;
+  typedef GenIntegralSet_1_1<CGShell,KineticOper,EmptySet> Kinetic_1_1_sq;
+  typedef GenIntegralSet_1_1<CGF,KineticOper,EmptySet> Kinetic_1_1_int;
 #endif
 
   //////////////////////////
@@ -55,6 +57,8 @@ namespace libint2 {
 #if LIBINT_SUPPORT_ONEBODYINTS
   Overlap_1_1_sq,
   Overlap_1_1_int,
+  Kinetic_1_1_sq,
+  Kinetic_1_1_int,
 #endif
   TwoPRep_11_11_sq,
   TwoPRep_11_11_int,

src/bin/libint/master_rrs_list.h
@@ -78,6 +78,10 @@ namespace libint2 {
   typedef VRR_1_Overlap_1<CGF,InBra> VRR_a_1_Overlap_1_int;
   typedef VRR_1_Overlap_1<CGShell,InKet> VRR_b_1_Overlap_1_sh;
   typedef VRR_1_Overlap_1<CGF,InKet> VRR_b_1_Overlap_1_int;
+  typedef VRR_1_Kinetic_1<CGShell,InBra> VRR_a_1_Kinetic_1_sh;
+  typedef VRR_1_Kinetic_1<CGF,InBra> VRR_a_1_Kinetic_1_int;
+  typedef VRR_1_Kinetic_1<CGShell,InKet> VRR_b_1_Kinetic_1_sh;
+  typedef VRR_1_Kinetic_1<CGF,InKet> VRR_b_1_Kinetic_1_int;
 #endif
 
   typedef VRR_11_TwoPRep_11<CGShell,0,InBra> VRR_a_11_TwoPRep_11_sh;

src/bin/libint/onep_1_1.h
@@ -27,6 +27,24 @@ namespace libint2 {
       return false;
   }
 
+  /// (s|T|s) shell quartet is not precomputed, but the integral is
+  template <>
+  inline bool
+  GenIntegralSet_1_1<CGF,KineticOper,EmptySet>::this_precomputed() const
+  {
+    /// uncontracted (s|s) are precomputed
+    if (parent_type::bra_.member(0,0).zero() &&
+        parent_type::ket_.member(0,0).zero() &&
+        parent_type::bra_.member(0,0).contracted() == false &&
+        parent_type::ket_.member(0,0).contracted() == false &&
+        parent_type::bra_.member(0,0).deriv().zero() &&
+        parent_type::ket_.member(0,0).deriv().zero()
+       )
+      return true;
+    else
+      return false;
+  }
+
 };
 
 #endif

src/bin/libint/oper.h
@@ -212,6 +212,19 @@ namespace libint2 {
   };
   typedef GenOper<Overlap_Descr> OverlapOper;
 
+  /** Kinetic is a one-body kinetic energy operator
+  */
+  struct Kinetic_Descr : public Contractable<Kinetic_Descr> {
+    typedef Nonmultiplicative1Body_Props Properties;
+    static const unsigned int max_key = 1;
+    unsigned int key() const { return 0; }
+    std::string description() const { return "Kinetic"; }
+    std::string label() const { return "Kinetic"; }
+    int psymm(int i, int j) const { assert(false); }
+    int hermitian(int i) const { return +1; }
+  };
+  typedef GenOper<Kinetic_Descr> KineticOper;
+
   /** TwoPRep is the two-body repulsion operator.
   */
   struct TwoPRep_Descr : public Contractable<TwoPRep_Descr> {

src/bin/libint/strategy.cc
@@ -261,6 +261,18 @@ namespace libint2 {
       > value;
     };
 # endif // strategy: A0 or 0B
+  template <> struct MasterStrategy<Kinetic_1_1_sq> {
+      typedef mpl::list<
+      VRR_a_1_Kinetic_1_sh,
+      VRR_b_1_Kinetic_1_sh
+      > value;
+    };
+  template <> struct MasterStrategy<Kinetic_1_1_int> {
+      typedef mpl::list<
+      VRR_a_1_Kinetic_1_int,
+      VRR_b_1_Kinetic_1_int
+      > value;
+    };
 #endif // LIBINT_SUPPORT_ONEBODYINTS
 
   /// transform<RRType> encapsulates RRType and the action associated with RRType

commit d7acd12a5e3568e22170d317adbc397de8998d14

Fixed bug introduced in [8cc13f7]

src/lib/libint/engine.h
@@ -310,7 +310,9 @@ namespace libint2 {
         primdata.AB_z[0] = A[2] - B[2];
 #endif
         }
-        else { // always VRR on ket, HRR to ket (overlap, coulomb), or no HRR at all (kinetic energy ints)
+        if (LIBINT2_SHELLQUARTET_SET != LIBINT2_SHELLQUARTET_SET_STANDARD
+            || type_ == kinetic)
+        { // always VRR on ket, HRR to ket (overlap, coulomb), or kinetic energy ints
 
 #if LIBINT2_DEFINED(eri,PB_x)
         primdata.PB_x[0] = Px - B[0];