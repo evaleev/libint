027937bddcdd683d0af13c54f39f6cd37ab0988c
Eduard Valeyev <eduard@valeyev.net>
2014-11-17 22:37:53 -0500

    travis-ci: cleanup

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -6,12 +6,3 @@ sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get update
 sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev
-
-if [ "$CXX" = "g++" ]; then
-    export CC=/usr/bin/gcc-$GCC_VERSION
-    export CXX=/usr/bin/g++-$GCC_VERSION
-else
-    export CFLAGS="--gcc-toolchain=/usr/bin/gcc-$GCC_VERSION"
-    export CXXFLAGS="--gcc-toolchain=/usr/bin/g++-$GCC_VERSION"
-fi
-echo $CXX
