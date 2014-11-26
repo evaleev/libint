8cfd95ae2b863970f8cd6994afcae9c79f7f3b17
Eduard Valeyev <eduard@valeyev.net>
2014-11-26 08:48:36 -0500

    Added an openmp-based Fock builder; default is openmp is available

--- tests/hartree-fock/hartree-fock.cc
+++ tests/hartree-fock/hartree-fock.cc
@@ -41,6 +41,10 @@
 #include <libint2.h>
 #include <libint2/cxxapi.h>
 
+#if defined(_OPENMP)
+# include <omp.h>
+#endif
+
 typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
         Matrix;  // import dense, dynamically sized Matrix type from Eigen;
                  // this is a matrix with row-major storage (http://en.wikipedia.org/wiki/Row-major_order)
@@ -66,6 +70,12 @@ Matrix compute_2body_fock_simple(const std::vector<libint2::Shell>& shells,
 // an efficient Fock builder; *integral-driven* hence computes permutationally-unique ints once
 Matrix compute_2body_fock(const std::vector<libint2::Shell>& shells,
                                  const Matrix& D);
+// a threaded Fock builder
+#if defined(_OPENMP)
+Matrix compute_2body_fock_openmp(const std::vector<libint2::Shell>& shells,
+                                 const Matrix& D);
+#endif
+
 
 int main(int argc, char *argv[]) {
 
@@ -191,7 +201,11 @@ int main(int argc, char *argv[]) {
       // build a new Fock matrix
       auto F = H;
       //F += compute_2body_fock_simple(shells, D);
+#if defined(_OPENMP)
+      F += compute_2body_fock_openmp(shells, D);
+#else
       F += compute_2body_fock(shells, D);
+#endif
 
       if (iter == 1) {
         cout << "\n\tFock Matrix:\n";
@@ -766,3 +780,111 @@ Matrix compute_2body_fock(const std::vector<libint2::Shell>& shells,
   Matrix Gt = G.transpose();
   return 0.5 * (G + Gt);
 }
+
+#if defined(_OPENMP)
+Matrix compute_2body_fock_openmp(const std::vector<libint2::Shell>& shells,
+                                 const Matrix& D) {
+
+  const auto n = nbasis(shells);
+  const auto nthreads = omp_get_max_threads();
+  std::vector<Matrix> G(nthreads, Matrix::Zero(n,n));
+
+  // construct the 2-electron repulsion integrals engine
+  typedef libint2::TwoBodyEngine<libint2::Coulomb> coulomb_engine_type;
+  std::vector<coulomb_engine_type> engines(nthreads);
+  engines[0] = coulomb_engine_type(max_nprim(shells), max_l(shells), 0);
+  for(size_t i=1; i!=nthreads; ++i) {
+    engines[i] = engines[0];
+  }
+
+  auto shell2bf = map_shell_to_basis_function(shells);
+
+#pragma omp parallel
+  {
+    auto thread_id = omp_get_thread_num();
+
+    // loop over permutationally-unique set of shells
+    for(auto s1=0, s1234=0; s1!=shells.size(); ++s1) {
+
+      auto bf1_first = shell2bf[s1]; // first basis function in this shell
+      auto n1 = shells[s1].size();// number of basis functions in this shell
+
+      for(auto s2=0; s2<=s1; ++s2) {
+
+        auto bf2_first = shell2bf[s2];
+        auto n2 = shells[s2].size();
+
+        for(auto s3=0; s3<=s1; ++s3) {
+
+          auto bf3_first = shell2bf[s3];
+          auto n3 = shells[s3].size();
+
+          const auto s4_max = (s1 == s3) ? s2 : s3;
+          for(auto s4=0; s4<=s4_max; ++s4, ++s1234) {
+
+            if (s1234 % nthreads != thread_id)
+              continue;
+
+            auto bf4_first = shell2bf[s4];
+            auto n4 = shells[s4].size();
+
+            // compute the permutational degeneracy (i.e. # of equivalents) of the given shell set
+            auto s12_deg = (s1 == s2) ? 1.0 : 2.0;
+            auto s34_deg = (s3 == s4) ? 1.0 : 2.0;
+            auto s12_34_deg = (s1 == s3) ? (s2 == s4 ? 1.0 : 2.0) : 2.0;
+            auto s1234_deg = s12_deg * s34_deg * s12_34_deg;
+
+            const auto* buf = engines[thread_id].compute(shells[s1], shells[s2], shells[s3], shells[s4]);
+
+            // 1) each shell set of integrals contributes up to 6 shell sets of the Fock matrix:
+            //    F(a,b) += (ab|cd) * D(c,d)
+            //    F(c,d) += (ab|cd) * D(a,b)
+            //    F(b,d) -= 1/4 * (ab|cd) * D(a,c)
+            //    F(b,c) -= 1/4 * (ab|cd) * D(a,d)
+            //    F(a,c) -= 1/4 * (ab|cd) * D(b,d)
+            //    F(a,d) -= 1/4 * (ab|cd) * D(b,c)
+            // 2) each permutationally-unique integral (shell set) must be scaled by its degeneracy,
+            //    i.e. the number of the integrals/sets equivalent to it
+            // 3) the end result must be symmetrized
+            for(auto f1=0, f1234=0; f1!=n1; ++f1) {
+              const auto bf1 = f1 + bf1_first;
+              for(auto f2=0; f2!=n2; ++f2) {
+                const auto bf2 = f2 + bf2_first;
+                for(auto f3=0; f3!=n3; ++f3) {
+                  const auto bf3 = f3 + bf3_first;
+                  for(auto f4=0; f4!=n4; ++f4, ++f1234) {
+                    const auto bf4 = f4 + bf4_first;
+
+                    const auto value = buf[f1234];
+
+                    const auto value_scal_by_deg = value * s1234_deg;
+
+                    auto& g = G[thread_id];
+
+                    g(bf1,bf2) += D(bf3,bf4) * value_scal_by_deg;
+                    g(bf3,bf4) += D(bf1,bf2) * value_scal_by_deg;
+                    g(bf1,bf3) -= 0.25 * D(bf2,bf4) * value_scal_by_deg;
+                    g(bf2,bf4) -= 0.25 * D(bf1,bf3) * value_scal_by_deg;
+                    g(bf1,bf4) -= 0.25 * D(bf2,bf3) * value_scal_by_deg;
+                    g(bf2,bf3) -= 0.25 * D(bf1,bf4) * value_scal_by_deg;
+                  }
+                }
+              }
+            }
+
+          }
+        }
+      }
+    }
+  } // omp parallel
+
+  // accumulate contributions from all threads
+  for(size_t i=1; i!=nthreads; ++i) {
+    G[0] += G[i];
+  }
+
+  // symmetrize the result and return
+  return 0.5 * (G[0] + G[0].transpose());
+}
+#endif // _OPENMP
+
