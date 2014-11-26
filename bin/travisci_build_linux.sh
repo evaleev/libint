d05753ced5b5cb67294d7ef4bb78656caf67df72
Eduard Valeyev <eduard@valeyev.net>
2014-11-26 10:16:09 -0500

    travis-ci: added tests for exported library

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -17,3 +17,14 @@ export LDFLAGS=$OPENMPFLAGS
 ./configure CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
 make -j2
 make check
+
+make export
+mkdir export_build
+mv libint-*.tgz export_build
+cd export_build
+tar -xvzf libint-*.tgz
+rm -f libint-*.tgz
+cd libint-*
+./configure CPPFLAGS='-I/usr/include/eigen3'
+make -j2
+make check
