2c2e31a1d4cf235255575046a2dfe58bde7273b7
Eduard Valeyev <eduard@valeyev.net>
2014-11-26 09:23:18 -0500

    travis-ci: trying -openmp for clang

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -5,9 +5,12 @@ set -e
 if [ "$CXX" = "g++" ]; then
     export CC=/usr/bin/gcc-$GCC_VERSION
     export CXX=/usr/bin/g++-$GCC_VERSION
+    export OPENMPFLAGS=-fopenmp
+else
+    export OPENMPFLAGS=-openmp
 fi
 
 ./autogen.sh
-./configure CXXFLAGS='-std=c++11 -fopenmp' LDFLAGS='-fopenmp' CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
+./configure CXXFLAGS='-std=c++11 $OPENMPFLAGS' LDFLAGS=$OPENMPFLAGS CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
 make -j2
 make check
