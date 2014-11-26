e0448ae333f4216e3a45cf6ff3b4628a4b976d93
Eduard Valeyev <eduard@valeyev.net>
2014-11-26 09:46:56 -0500

    travis-ci: more shell issues

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -9,8 +9,10 @@ if [ "$CXX" = "g++" ]; then
 else
     export OPENMPFLAGS=-openmp
 fi
+export CXXFLAGS=-std=c++11 $OPENMPFLAGS
+export LDFLAGS=$OPENMPFLAGS
 
 ./autogen.sh
-./configure CXXFLAGS="'-std=c++11 $OPENMPFLAGS'" LDFLAGS=$OPENMPFLAGS CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
+./configure CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
 make -j2
 make check
