c290b5c107bc0a948877e1b26155774c22573acd
Eduard Valeyev <eduard@valeyev.net>
2014-11-26 09:33:37 -0500

    travis-ci: battling shell

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -11,6 +11,6 @@ else
 fi
 
 ./autogen.sh
-./configure CXXFLAGS="-std=c++11 $OPENMPFLAGS" LDFLAGS=$OPENMPFLAGS CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
+./configure CXXFLAGS="'-std=c++11 $OPENMPFLAGS'" LDFLAGS=$OPENMPFLAGS CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
 make -j2
 make check
