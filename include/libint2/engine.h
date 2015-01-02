evaleev/libint

commit 32ca975c04b2f0d4f14a21457d8e270f78b20efd

Support fine-grained instrumented profiling of internals of OS build.

include/libint2/engine.h
@@ -35,7 +35,9 @@
 #include <Eigen/Core>
 
 // uncomment to time the TwoBodyEngine
-//#define LIBINT2_ENGINE_TIMERS
+#ifdef LIBINT2_PROFILE
+#  define LIBINT2_ENGINE_TIMERS
+#endif
 
 namespace libint2 {
 
@@ -687,7 +689,6 @@ namespace libint2 {
       }
 
 #ifdef LIBINT2_ENGINE_TIMERS
-      Timers<3> timers;
 #endif
 
       /// computes shell set of integrals
@@ -759,16 +760,16 @@ namespace libint2 {
           primdata_[0].contrdepth = p;
         }
 
+#ifdef LIBINT2_ENGINE_TIMERS
+          timers.stop(0);
+#endif
+
         // all primitive combinations screened out? return zeroes
         if (primdata_[0].contrdepth == 0) {
           const size_t n = bra1.size() * bra2.size() * ket1.size() * ket2.size();
           memset(primdata_[0].stack, 0, sizeof(LIBINT2_REALTYPE)*n);
           return primdata_[0].stack;
         }
-
-#ifdef LIBINT2_ENGINE_TIMERS
-        timers.stop(0);
-#endif
 
         LIBINT2_REALTYPE* result = nullptr;
 
@@ -922,6 +923,25 @@ namespace libint2 {
         }
       }
 
+      void print_timers() {
+#ifdef LIBINT2_ENGINE_TIMERS
+        std::cout << "timers: prereq = " << timers.read(0);
+#  ifndef LIBINT2_PROFILE // if libint's profiling was on, engine's build timer will include its overhead
+                          // do not report it, detailed profiling from libint will be printed below
+        std::cout << " build = " << timers.read(1);
+#  endif
+        std::cout << " tform = " << timers.read(2) << std::endl;
+#endif
+#ifdef LIBINT2_PROFILE
+        std::cout << "build timers: hrr = " << primdata_[0].timers->read(0)
+                << " vrr = " << primdata_[0].timers->read(1) << std::endl;
+#endif
+      }
+
+      void skip_core_ints(bool s) {
+        skip_core_ints_ = s;
+      }
+
     private:
 
       inline bool compute_primdata(Libint_t& primdata,
@@ -979,6 +999,14 @@ namespace libint2 {
               break;
             default: assert(deriv_order_ < 3);
         }
+
+#ifdef LIBINT2_ENGINE_TIMERS
+        timers.set_now_overhead(25);
+#endif
+#ifdef LIBINT2_PROFILE
+        primdata_[0].timers->set_now_overhead(25);
+#endif
+        skip_core_ints(false);
       }
 
       void finalize() {

configure.ac
@@ -818,6 +818,23 @@ esac
     AC_MSG_RESULT([FLOP counter will not be supported])
 ])
 
+AC_ARG_ENABLE(profile,
+[  --enable-profile        Turn on profiling instrumentation of the library. Generated code will require C++11!],
+[
+case $enableval in
+  yes)
+    AC_DEFINE(LIBINT_PROFILE)
+    AC_MSG_RESULT([Profiling instrumentation enabled (generated code will require C++11)])
+  ;;
+  no)
+    AC_MSG_RESULT([Profiling instrumentation disabled])
+  ;;
+esac
+],
+[
+    AC_MSG_RESULT([Profiling instrumentation disabled])
+])
+
 acx_contracted_ints=yes
 AC_ARG_ENABLE(contracted-ints,
 [  --enable-contracted-ints     Turn on support for contracted integrals.],

include/libint2/config.h.in
@@ -171,6 +171,9 @@
 /* Whether FLOP counting is supported */
 #undef LIBINT_FLOP_COUNT
 
+/* Whether profile instrumentation will be enabled */
+#undef LIBINT_PROFILE
+
 /* Support contracted integrals? */
 #undef LIBINT_CONTRACTED_INTS
 

include/libint2/timer.h
@@ -19,9 +19,7 @@
 #ifndef _libint2_src_lib_libint_timer_h_
 #define _libint2_src_lib_libint_timer_h_
 
-#if __cplusplus <= 199711L
-# error "The simple Libint API requires C++11 support"
-#endif
+#if __cplusplus > 199711L
 
 #include <chrono>
 
@@ -36,6 +34,11 @@ namespace libint2 {
       typedef std::chrono::duration<double> dur_t;
       typedef std::chrono::high_resolution_clock clock_t;
       typedef std::chrono::time_point<clock_t> time_point_t;
+
+      Timers() {
+        clear();
+        set_now_overhead(0);
+      }
 
       static time_point_t now() {
         return clock_t::now();
@@ -75,6 +78,8 @@ namespace libint2 {
   };
 
 } // namespace libint2
+
+#endif // C++11 or later
 
 #endif // header guard
 

src/bin/libint/build_libint.cc
@@ -577,6 +577,9 @@ void try_main (int argc, char* argv[])
 #if LIBINT_FLOP_COUNT
   cparams->count_flops(true);
 #endif
+#if LIBINT_PROFILE
+  cparams->profile(true);
+#endif
 #if LIBINT_ACCUM_INTS
   cparams->accumulate_targets(true);
 #else
@@ -825,6 +828,10 @@ build_TwoPRep_2b_2k(std::ostream& os, const SafePtr<CompilationParameters>& cpar
           //dg_xxxx->registry()->condense_expr(true);
           // Need to accumulate integrals?
           dg_xxxx->registry()->accumulate_targets(cparams->accumulate_targets());
+          // need to profile?
+          if (cparams->profile()) {
+            dg_xxxx->registry()->current_timer(0);
+          }
 
           ////////////
           // loop over unique derivative index combinations

src/bin/libint/buildtest.h
@@ -296,6 +296,9 @@ namespace libint2 {
       dg_prereq->registry()->return_targets(false);
       dg_prereq->registry()->accumulate_targets(true);
       dg_prereq->registry()->stack_name("stack");
+      if (dg->registry()->current_timer() >= 0) {
+        dg_prereq->registry()->current_timer( dg->registry()->current_timer() + 1 );
+      }
 
       // now is the "right" time to reset dg
       // reset graph of the previous computation so that the vertices that will be targets on the new graph

src/bin/libint/default_params.cc
@@ -39,6 +39,7 @@ CompilationParameters::CompilationParameters() :
   single_evaltype_(Defaults::single_evaltype),
   use_C_linking_(Defaults::use_C_linking),
   count_flops_(Defaults::count_flops),
+  profile_(Defaults::profile),
   accumulate_targets_(Defaults::accumulate_targets),
   realtype_(Defaults::realtype),
   contracted_targets_(Defaults::contracted_targets)
@@ -77,6 +78,7 @@ CompilationParameters::print(std::ostream& os) const
   os << "API_PREFIX           = " << api_prefix() << endl;
   os << "USE_C_LINKING        = " << (use_C_linking() ? "true" : "false") << endl;
   os << "COUNT_FLOPS          = " << (count_flops() ? "true" : "false") << endl;
+  os << "PROFILE              = " << (profile() ? "true" : "false") << endl;
   os << "ACCUMULATE_TARGETS   = " << (accumulate_targets() ? "true" : "false") << endl;
   os << "REALTYPE             = " << (realtype()) << endl;
   os << "CONTRACTED_TARGETS   = " << (contracted_targets() ? "true" : "false") << endl;

src/bin/libint/default_params.h
@@ -85,6 +85,10 @@ namespace libint2 {
     bool count_flops() const {
       return count_flops_;
     }
+    /// whether profiling instrumentation is enabled
+    bool profile() const {
+      return profile_;
+    }
     /// whether target integrals are accumulated
     bool accumulate_targets() const {
       return accumulate_targets_;
@@ -144,6 +148,10 @@ namespace libint2 {
     void count_flops(bool a) {
       count_flops_ = a;
     }
+    /// set to instrument profiling
+    void profile(bool a) {
+      profile_ = a;
+    }
     /// accumulate targets?
     void accumulate_targets(bool a) {
       accumulate_targets_ = a;
@@ -190,6 +198,8 @@ namespace libint2 {
       static const bool use_C_linking = true;
       /// Do not count FLOPs by default
       static const bool count_flops = false;
+      /// Do not profile by default
+      static const bool profile = false;
       /// Do not accumulate targets by default
       static const bool accumulate_targets = false;
       /// Use double for computations
@@ -237,6 +247,8 @@ namespace libint2 {
     bool use_C_linking_;
     /// whether to count FLOPs
     bool count_flops_;
+    /// whether to turn on profiling instrumentation
+    bool profile_;
     /// whether to accumulate targets
     bool accumulate_targets_;
     /// name of the floating-point type

commit 2bb1f71c27f46d2307ae4b96485296a531f719cc

Fixed bug introduced in 32ca975c04b2f0d4f14a21457d8e270f78b20efd

include/libint2/engine.h
@@ -941,10 +941,6 @@ namespace libint2 {
 #endif
       }
 
-      void skip_core_ints(bool s) {
-        skip_core_ints_ = s;
-      }
-
     private:
 
       inline bool compute_primdata(Libint_t& primdata,
@@ -1009,7 +1005,6 @@ namespace libint2 {
 #ifdef LIBINT2_PROFILE
         primdata_[0].timers->set_now_overhead(25);
 #endif
-        skip_core_ints(false);
       }
 
       void finalize() {