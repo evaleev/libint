8982b56812ab20174fd4f91d2328442e4a42a201
Eduard Valeyev <eduard@valeyev.net>
2015-02-07 10:54:49 -0500

    Minor tweak of travis linux script.

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -7,11 +7,12 @@ if [ "$CXX" = "g++" ]; then
     export CXX=/usr/bin/g++-$GCC_VERSION
     export OPENMPFLAGS=-fopenmp
 else
-    # not sure about clang OpenMP flags ... ignores -fopenmp and bombs with -openmp
+    # no OpenMP support in clang, will use C++11 threads
     export OPENMPFLAGS=
 fi
 export CXXFLAGS="-std=c++11 $OPENMPFLAGS"
 export LDFLAGS=$OPENMPFLAGS
+export LIBINT_NUM_THREADS=2
 
 ./autogen.sh
 ./configure CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
@@ -26,6 +27,5 @@ tar -xvzf libint-*.tgz
 rm -f libint-*.tgz
 cd libint-*
 ./configure CPPFLAGS='-I/usr/include/eigen3'
-# build currently does not work
 make -j2
 make check
