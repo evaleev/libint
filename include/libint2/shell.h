evaleev/libint

commit 4784c89b70a558c4043264e585076c5591cfbaeb

1) no need for explicit renorm() call any longer 2) Unit-normalize shells by default ... to revert back to the old behavior do libint2::Shell::do_enforce_unit_normalization(false) 3) checking dipole and quadrupole values from hartree-fock++ less tightly since it turned out they were only converged to 10 digits.

include/libint2/shell.h
@@ -99,8 +99,21 @@ namespace libint2 {
       std::array<real_t, 3> O;   //!< origin
       std::vector<real_t> max_ln_coeff; //!< maximum ln of (absolute) contraction coefficient for each primitive
 
-      Shell& move(const std::array<real_t, 3> new_origin) {
-        O = new_origin;
+      Shell() = default;
+      Shell(const Shell&) = default;
+      Shell(Shell&&) = default;
+      Shell(std::vector<real_t> _alpha,
+            std::vector<Contraction> _contr,
+            std::array<real_t, 3> _O) :
+              alpha(std::move(_alpha)),
+              contr(std::move(_contr)),
+              O(std::move(_O)) {
+        // embed normalization factors into contraction coefficients
+        renorm();
+      }
+
+      Shell& move(std::array<real_t, 3> new_origin) {
+        O = std::move(new_origin);
         return *this;
       }
 
@@ -113,37 +126,6 @@ namespace libint2 {
         size_t s = 0;
         for(const auto& c: contr) { s += c.size(); }
         return s;
-      }
-
-      /// embeds normalization constants into contraction coefficients. Do this before computing integrals.
-      /// \note Must be done only once.
-      void renorm() {
-        using libint2::math::df_Kminus1;
-        const auto sqrt_Pi_cubed = real_t{5.56832799683170784528481798212};
-        const auto np = nprim();
-        for(auto& c: contr) {
-          assert(c.l <= 15); // due to df_Kminus1[] a 64-bit integer type; kinda ridiculous restriction anyway
-          for(auto p=0; p!=np; ++p) {
-            assert(alpha[p] >= 0.0);
-            if (alpha[p] != 0.) {
-              const auto two_alpha = 2 * alpha[p];
-              const auto two_alpha_to_am32 = pow(two_alpha,c.l+1) * sqrt(two_alpha);
-              const auto norm = sqrt(pow(2,c.l) * two_alpha_to_am32/(sqrt_Pi_cubed * df_Kminus1[2*c.l] ));
-
-              c.coeff[p] *= norm;
-            }
-          }
-        }
-
-        // update max log coefficients
-        max_ln_coeff.resize(np);
-        for(auto p=0; p!=np; ++p) {
-          real_t max_ln_c = - std::numeric_limits<real_t>::max();
-          for(auto& c: contr) {
-            max_ln_c = std::max(max_ln_c, log(std::abs(c.coeff[p])));
-          }
-          max_ln_coeff[p] = max_ln_c;
-        }
       }
 
       size_t ncontr() const { return contr.size(); }
@@ -188,6 +170,28 @@ namespace libint2 {
         }
       }
 
+      struct defaultable_boolean {
+          typedef enum {false_value=0,true_value=1,default_value=2} value_t;
+          defaultable_boolean() : value_(default_value) {}
+          defaultable_boolean(bool v) : value_(static_cast<value_t>(v?1:0)) {}
+          bool is_default() const { return value_ == default_value; }
+          operator bool() const { assert(value_ != default_value); return value_ == true_value; }
+        private:
+          value_t value_;
+      };
+
+      /// sets and/or reports whether the auto-renormalization to unity is set
+      /// if called without arguments, returns the current value of the flag
+      /// otherwise, will set the flag to \c flag
+      /// \note by default, shells WILL be re-normalized to unity
+      static bool do_enforce_unit_normalization(defaultable_boolean flag = defaultable_boolean()) {
+        static bool result{true};
+        if (not flag.is_default()) {
+          result = flag;
+        }
+        return result;
+      }
+
       /// @return "unit" Shell, with exponent=0. and coefficient=1., located at the origin
       static Shell unit() {
         libint2::Shell unitshell{
@@ -198,6 +202,58 @@ namespace libint2 {
         unitshell.renorm();
         return unitshell;
       }
+
+    private:
+      /// embeds normalization constants into contraction coefficients. Do this before computing integrals.
+      /// \warning Must be done only once.
+      /// \note this is now private
+      void renorm() {
+        using libint2::math::df_Kminus1;
+        const auto sqrt_Pi_cubed = real_t{5.56832799683170784528481798212};
+        const auto np = nprim();
+        for(auto& c: contr) {
+          assert(c.l <= 15); // due to df_Kminus1[] a 64-bit integer type; kinda ridiculous restriction anyway
+          for(auto p=0; p!=np; ++p) {
+            assert(alpha[p] >= 0.0);
+            if (alpha[p] != 0.) {
+              const auto two_alpha = 2 * alpha[p];
+              const auto two_alpha_to_am32 = pow(two_alpha,c.l+1) * sqrt(two_alpha);
+              const auto normalization_factor = sqrt(pow(2,c.l) * two_alpha_to_am32/(sqrt_Pi_cubed * df_Kminus1[2*c.l] ));
+
+              c.coeff[p] *= normalization_factor;
+            }
+          }
+
+          // need to force normalization to unity?
+          if (do_enforce_unit_normalization()) {
+            // compute the self-overlap of the , scale coefficients by its inverse square root
+            double norm{0};
+            for(auto p=0; p!=np; ++p) {
+              for(auto q=0; q<=p; ++q) {
+                auto gamma = alpha[p] + alpha[q];
+                norm += (p==q ? 1.0 : 2.0) * df_Kminus1[2*c.l] * sqrt_Pi_cubed * c.coeff[p] * c.coeff[q] /
+                        (pow(2,c.l) * pow(gamma,c.l+1) * sqrt(gamma));
+              }
+            }
+            auto normalization_factor = 1.0 / sqrt(norm);
+            for(auto p=0; p!=np; ++p) {
+              c.coeff[p] *= normalization_factor;
+            }
+          }
+
+        }
+
+        // update max log coefficients
+        max_ln_coeff.resize(np);
+        for(auto p=0; p!=np; ++p) {
+          real_t max_ln_c = - std::numeric_limits<real_t>::max();
+          for(auto& c: contr) {
+            max_ln_c = std::max(max_ln_c, log(std::abs(c.coeff[p])));
+          }
+          max_ln_coeff[p] = max_ln_c;
+        }
+      }
+
   };
 
   inline std::ostream& operator<<(std::ostream& os, const Shell& sh) {

include/libint2/basis.h.in
@@ -89,11 +89,6 @@ namespace libint2 {
 
         }
 
-        // technical step: rescale contraction coefficients to include primitive normalization coefficients
-        for(auto& s: *this) {
-          s.renorm();
-        }
-
         init();
       }
 

tests/hartree-fock/Makefile
@@ -36,8 +36,8 @@ check1:: $(TEST1)
 check2::  $(TEST2)
 	- ./$(TEST2) $(SRCDIR)/h2o.xyz | tee check2.log && \
  cat check2.log | awk ' /^\*\* Hartree/ { eref = -76.003354058456; e = $$5; tol = 1e-11; if (e - eref < tol && eref - e < tol) {print "HF energy check passed"; exit 0} else {print "HF energy check failed"; exit 1} }' && \
- cat check2.log | awk ' /^\*\* edipole/ { trmuref = -0.297869981927709; trmu = $$4 + $$5 + $$6; tol = 1e-11; if (trmu - trmuref < tol && trmuref - trmu < tol) {print "dipole moment check passed"; exit 0} else {print "dipole moment check failed"; exit 1} }' && \
- cat check2.log | awk ' /^\*\* equadrupole/ { trquref = -21.41570826291166; trqu = $$4 + $$7 + $$9; tol = 1e-11; if (trqu - trquref < tol && trquref - trqu < tol) {print "quadrupole moment check passed"; exit 0} else {print "quadrupole moment check failed"; exit 1} }' && \
+ cat check2.log | awk ' /^\*\* edipole/ { trmuref = -0.29786998277636906; trmu = $$4 + $$5 + $$6; tol = 1e-9; if (trmu - trmuref < tol && trmuref - trmu < tol) {print "dipole moment check passed"; exit 0} else {print "dipole moment check failed"; exit 1} }' && \
+ cat check2.log | awk ' /^\*\* equadrupole/ { trquref = -21.415708269322693; trqu = $$4 + $$7 + $$9; tol = 1e-9; if (trqu - trquref < tol && trquref - trqu < tol) {print "quadrupole moment check passed"; exit 0} else {print "quadrupole moment check failed"; exit 1} }' && \
  rm -f check2.txt
 
 $(TEST1): $(CXXTEST1OBJ) $(COMPILER_LIB) $(COMPUTE_LIB)

tests/hartree-fock/hartree-fock.cc
@@ -445,11 +445,6 @@ std::vector<Shell> make_sto3g_basis(const std::vector<Atom>& atoms) {
 
   }
 
-  // technical step: rescale contraction coefficients to include primitive normalization coefficients
-  for(auto& s: shells) {
-    s.renorm();
-  }
-
   return shells;
 }
 

commit 33a8aebe9caca524b9f10854c82cce0e3d58e878

Fixed problem with Shell::unit() introduced in 4784c89b70a558c4043264e585076c5591cfbaeb

include/libint2/shell.h
@@ -194,12 +194,11 @@ namespace libint2 {
 
       /// @return "unit" Shell, with exponent=0. and coefficient=1., located at the origin
       static Shell unit() {
-        libint2::Shell unitshell{
-                    {0.0}, // exponent
-                    {{0, false, {1.0}}},
-                    {{0.0, 0.0, 0.0}} // placed at origin
-                };
-        unitshell.renorm();
+        libint2::Shell unitshell;
+        unitshell.alpha.emplace_back(0.0);                           // exponent = 0
+        unitshell.contr.emplace_back(Contraction{0, false, {1.0}});  // contraction coefficient = 1
+        unitshell.O = std::array<real_t,3>({0.0, 0.0, 0.0});         // placed at origin
+        unitshell.max_ln_coeff.emplace_back(1.0);
         return unitshell;
       }
 