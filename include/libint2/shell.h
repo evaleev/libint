evaleev/libint

commit 4eb2485046d051638342a886f6642cb265a0aa96

Amended 2d4d0b59ff4372bfed44776b3fa78a6db71cfb6d to satisfy gcc 4.7

include/libint2/shell.h
@@ -205,10 +205,10 @@ namespace libint2 {
       // this makes a unit shell
       struct make_unit{};
       Shell(make_unit) :
-        alpha{1, 0.0},                           // exponent = 0
-        contr{1, Contraction{0, false, {1.0}}},  // contraction coefficient = 1
+        alpha{10.0},                           // exponent = 0
+        contr{Contraction{0, false, {1.0}}},  // contraction coefficient = 1
         O{0.0, 0.0, 0.0},                      // placed at origin
-        max_ln_coeff{1, 0.0} {
+        max_ln_coeff{0.0} {
       }
 
       /// embeds normalization constants into contraction coefficients. Do this before computing integrals.

commit d945e06e83b1d2e646463569ea03aee307b2df62

fixed stupid typo introduced in 4eb2485046d051638342a886f6642cb265a0aa96

include/libint2/shell.h
@@ -205,9 +205,9 @@ namespace libint2 {
       // this makes a unit shell
       struct make_unit{};
       Shell(make_unit) :
-        alpha{10.0},                           // exponent = 0
+        alpha{0.0},                           // exponent = 0
         contr{Contraction{0, false, {1.0}}},  // contraction coefficient = 1
-        O{{0.0, 0.0, 0.0}},                      // placed at origin
+        O{{0.0, 0.0, 0.0}},                   // placed at origin
         max_ln_coeff{0.0} {
       }
 