6f0152fdbbeb96378b75af6e10734ff41d735ddf
Eduard Valeyev <eduard@valeyev.net>
2015-11-16 18:53:17 -0500

    travis-ci: enable validation of 1-body derivs in HF++ code

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -15,7 +15,7 @@ export LDFLAGS=$OPENMPFLAGS
 export LIBINT_NUM_THREADS=2
 
 ./autogen.sh
-./configure CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1
+./configure CPPFLAGS='-I/usr/include/eigen3' --with-max-am=2,2 --with-eri-max-am=2,1 --with-eri3-max-am=3,2 --enable-eri=1 --enable-eri3=1 --enable-1body=1
 make -j2
 make check
 cd src/bin/test_eri; ./stdtests.pl; cd ../../..
