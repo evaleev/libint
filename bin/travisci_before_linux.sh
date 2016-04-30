532d97ebc16f8b7e00a0ff33ab9502458efcb2f5
Eduard Valeyev <eduard@valeyev.net>
2016-04-29 22:37:00 -0400

    Update travisci_before_linux.sh

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-
+sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main'
 sudo wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+
 sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get update
