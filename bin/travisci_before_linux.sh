c6be1161c8dd0b5e4e5ec1bd749a8709b909b101
Eduard Valeyev <eduard@valeyev.net>
2016-04-29 22:45:05 -0400

    Update travisci_before_linux.sh

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -8,6 +8,5 @@ sudo wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get update
-#sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev
 
-sudo apt-get install -y g++-$GCC_VERSION llvm-3.7 llvm-3.7-dev libgmp-dev libeigen3-dev libboost1.55-dev
+sudo apt-get install -y g++-$GCC_VERSION clang-3.7 libgmp-dev libeigen3-dev libboost1.55-dev
