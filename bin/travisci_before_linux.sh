884910451c385e99c8cdeee2a23c304e6d726388
Eduard Valeyev <eduard@valeyev.net>
2016-04-29 21:37:55 -0400

    travis-ci: upgrade clang to 3.8

--- bin/travisci_before_linux.sh
+++ bin/travisci_before_linux.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+
+sudo wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo add-apt-repository ppa:boost-latest/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get update
-sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev
+#sudo apt-get install -y g++-$GCC_VERSION llvm-3.4 llvm-3.4-dev libgmp-dev libeigen3-dev libboost1.55-dev
+
+apt-get install clang-3.8
+sudo apt-get install -y g++-$GCC_VERSION llvm-3.8 llvm-3.8-dev libgmp-dev libeigen3-dev libboost1.55-dev
