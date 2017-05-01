46b988e523a57ed7b0992a18454077bc52ba262f
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-04-30 23:52:08 -0400

    touched up the copyright header

--- src/bin/test_eri/test_eri_rys_vec.cc
+++ src/bin/test_eri/test_eri_rys_vec.cc
@@ -14,7 +14,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Libint.  If not, see http://www.gnu.org/licenses/.
+ *  along with Libint.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #include "simd_wrapped_vector.hpp"
