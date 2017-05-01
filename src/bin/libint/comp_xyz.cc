839ffb995cb22f9cb687af00750b389a832aa12f
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-04-30 23:47:37 -0400

    Added missing * in copyright header

--- src/bin/libint/comp_xyz.cc
+++ src/bin/libint/comp_xyz.cc
@@ -1,7 +1,7 @@
 /*
  *  Copyright (C) 2004-2017 Edward F. Valeev
-
- *  This file is part of Libint.
+ *
+ * This file is part of Libint.
  *
  *  Libint is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
