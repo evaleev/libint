16f6b5d48ea4fc8f87aa7484f233a03daefe019d
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-05-02 12:27:13 -0400

    removed old (public domain) any

--- include/libint2/util/any.h
+++ include/libint2/util/any.h
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2004-2017 Edward F. Valeev
+ *
+ *  This file is part of Libint.
+ *
+ *  Libint is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Libint is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Libint.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef _libint2_include_libint2_util_any_h_
 #define _libint2_include_libint2_util_any_h_
 
@@ -8,148 +28,6 @@
 #include <cassert>
 
 namespace libint2 {
-
-#define LIBINT2_USES_OLD_ANY 0
-#if LIBINT2_USES_OLD_ANY
-// taken from http://codereview.stackexchange.com/questions/20058/c11-any-class
-// and modified slightly.
-// this code is in public domain
-
-  struct any
-  {
-    public:
-
-      template<class T>
-      using StorageType = typename std::decay<T>::type;
-
-      /// constructs a default any object which evaluates to false
-      any() = default;
-      any(const any& that) : handle_(that.clone()) {}
-      any(any&& that) : handle_(std::move(that.handle_)) { }
-      template <typename U,
-                typename = typename std::enable_if<not std::is_same<
-                    any, typename std::decay<U>::type>::value>::type>
-      any(U&& value)
-          : handle_(new handle<StorageType<U>>(std::forward<U>(value))) {}
-
-      any& operator=(const any& a)
-      {
-        any tmp(a);
-        std::swap(*this, tmp);
-        return *this;
-      }
-      template <typename U,
-                typename = typename std::enable_if<not std::is_same<
-                    any, typename std::decay<U>::type>::value>::type>
-      any& operator=(U a) {
-        any tmp(std::forward<U>(a));
-        std::swap(*this, tmp);
-        return *this;
-      }
-      any& operator=(any&& a) {
-        std::swap(handle_, a.handle_);
-        return *this;
-      }
-
-      bool empty() const { return handle_ == nullptr; }
-
-      template<class U> bool is() const
-      {
-          typedef StorageType<U> T;
-          auto derived = dynamic_cast<handle<T>*> (handle_.get());
-          return derived;
-      }
-
-      /// \note if NDEBUG is not defined, will throw \c std::bad_cast if U is not the stored type
-      template<class U>
-      StorageType<U>& as()
-      {
-          typedef StorageType<U> T;
-
-#if not defined(NDEBUG)
-          auto derived = dynamic_cast<handle<T>*> (handle_.get());
-          if (!derived)
-              throw std::bad_cast();
-#else // NDEBUG
-          auto derived = static_cast<handle<T>*> (handle_.get());
-#endif
-
-          return derived->value;
-      }
-
-      /// \note if NDEBUG is not defined, will throw \c std::bad_cast if U is not the stored type
-      template<class U>
-      const StorageType<U>& as() const
-      {
-          typedef StorageType<U> T;
-
-#if not defined(NDEBUG)
-          auto derived = dynamic_cast<handle<T>*> (handle_.get());
-          if (!derived)
-              throw std::bad_cast();
-#else // NDEBUG
-          auto derived = static_cast<handle<T>*> (handle_.get());
-#endif
-
-          return derived->value;
-      }
-
-      template<class U>
-      explicit operator U()
-      {
-          return as<StorageType<U>>();
-      }
-
-
-    private:
-      struct handle_base
-      {
-          virtual ~handle_base() {}
-
-          virtual handle_base* clone() const = 0;
-      };
-
-      template<typename T>
-      struct handle : handle_base
-      {
-          template<typename U> handle(U&& value) : value(std::forward<U>(value)) { }
-
-          T value;
-
-          handle_base* clone() const { return new handle<T>(value); }
-      };
-
-      handle_base* clone() const
-      {
-          if (handle_)
-              return handle_->clone();
-          else
-              return nullptr;
-      }
-
-      std::unique_ptr<handle_base> handle_;
-  };
-#else  // LIBINT2_USES_OLD_ANY
-
-  /*
-   *  Copyright (C) 2004-2017 Edward F. Valeev
-   *
-   *  This file is part of Libint.
-   *
-   *  Libint is free software: you can redistribute it and/or modify
-   *  it under the terms of the GNU Lesser General Public License as published by
-   *  the Free Software Foundation, either version 3 of the License, or
-   *  (at your option) any later version.
-   *
-   *  Libint is distributed in the hope that it will be useful,
-   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   *  GNU Lesser General Public License for more details.
-   *
-   *  You should have received a copy of the GNU Lesser General Public License
-   *  along with Libint.  If not, see <http://www.gnu.org/licenses/>.
-   *
-   */
 
 // use C++17 std::any, if available
 #if __cplusplus >= 201703L
@@ -309,8 +187,6 @@ ValueType any_cast(any& operand) {
   throw bad_any_cast();
 }
 #endif  // C++17
-
-#endif  // LIBINT2_USES_OLD_ANY
 
 } // namespace libint2
 
