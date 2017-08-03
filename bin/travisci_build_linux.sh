a14427d53928f21c1a2f2bfeffea1a7734686518
Eduard Valeyev <eduard.valeyev@gmail.com>
2017-08-03 16:13:37 -0400

    old boost.pp reports that clang does not have variadic macros (erroneously), hack around this

--- bin/travisci_build_linux.sh
+++ bin/travisci_build_linux.sh
@@ -6,13 +6,16 @@ if [ "$CXX" = "g++" ]; then
     export CC=/usr/bin/gcc-$GCC_VERSION
     export CXX=/usr/bin/g++-$GCC_VERSION
     export OPENMPFLAGS=-fopenmp
+    export EXTRAFLAGS=
 else
     # no OpenMP support in clang, will use C++11 threads
     export OPENMPFLAGS=
     export CC=/usr/bin/clang-5.0
     export CXX=/usr/bin/clang++-5.0
+    # Boost 1.55 is too old, override Boost.PP detection of variadic macro support
+    export EXTRAFLAGS="-DBOOST_PP_VARIADICS=1"
 fi
-export CXXFLAGS="-std=c++11 -Wno-enum-compare $OPENMPFLAGS"
+export CXXFLAGS="-std=c++11 -Wno-enum-compare $OPENMPFLAGS $EXTRAFLAGS"
 export LDFLAGS=$OPENMPFLAGS
 export LIBINT_NUM_THREADS=2
 
