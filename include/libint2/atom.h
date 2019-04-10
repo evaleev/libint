56e884fa7dbeb3d6a41b39bc21c44b1821618c95
Eduard Valeyev <eduard@valeyev.net>
2019-04-10 10:09:56 -0400

    merged master into pbc

--- include/libint2/atom.h
+++ include/libint2/atom.h
@@ -41,14 +41,6 @@ namespace libint2 {
       double x, y, z;
   };
 
-  namespace {
-    bool strcaseequal(const std::string& a, const std::string& b) {
-      return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin(),
-                                                [](char a, char b) {return ::tolower(a) == ::tolower(b);}
-                                               );
-    }
-  }
-
   namespace constants {
   /// the 2014 CODATA reference set, available at DOI 10.1103/RevModPhys.88.035009
   struct codata_2014 {
@@ -62,86 +54,168 @@ namespace libint2 {
   };
   }  // namespace constants
 
-  /// reads the list of atoms from a file in the standard XYZ format supported
-  /// by most chemistry software (see <a
-  /// href="https://en.wikipedia.org/wiki/XYZ_file_format">the Wikipedia
-  /// page</a>)
-  /// \param is[in] the std::istream object from which the data will be read
-  /// \param bohr_to_angstrom[in] the conversion factor from Bohr (atomic unit
-  /// of length; Libint uses atomic units throughout) to angstrom (in which
-  /// the Cartesian coordinates are given in the XYZ file). The default is
-  /// the 2010 CODATA value given by the
-  /// libint2::constants::codata_2010::bohr_to_angstrom
-  /// constant.
-  /// \return a std::vector of Atom objects
-  /// \throw std::logic_error if cannot parse the contents of \c is
-  inline std::vector<Atom> read_dotxyz(
-      std::istream& is,
-      const double bohr_to_angstrom = constants::codata_2010::bohr_to_angstrom) {
-    const double angstrom_to_bohr = 1 / bohr_to_angstrom;
-    // first line = # of atoms
-    size_t natom;
-    is >> natom;
-    // read off the rest of first line and discard
-    std::string rest_of_line;
-    std::getline(is, rest_of_line);
+} // namespace libint2
 
-    // second line = comment
-    std::string comment;
-    std::getline(is, comment);
+namespace {
 
-    // rest of lines are atoms
-    std::vector<Atom> atoms(natom);
-    for (size_t i = 0; i < natom; i++) {
-      // read line
-      std::string line;
-      std::getline(is, line);
-      std::istringstream iss(line);
-      // then parse ... this handles "extended" XYZ formats
-      std::string element_symbol;
-      double x, y, z;
-      iss >> element_symbol >> x >> y >> z;
+bool strcaseequal(const std::string &a, const std::string &b) {
+  return a.size() == b.size() &&
+         std::equal(a.begin(), a.end(), b.begin(), [](char a, char b) {
+           return ::tolower(a) == ::tolower(b);
+         });
+}
 
-      // .xyz files report element labels, hence convert to atomic numbers
+/// reads the list of atoms from a file in the standard or PBC-extended XYZ format \sa libint2::read_dotxyz \sa libint2::read_dotxyz_pbc
+inline std::tuple<std::vector<libint2::Atom>,
+                  std::array<std::array<double, 3>, 3>>
+__libint2_read_dotxyz(std::istream &is, const double bohr_to_angstrom,
+                      const bool pbc = false) {
+  using libint2::Atom;
+
+  // first line = # of atoms
+  size_t natom;
+  is >> natom;
+
+  // read off the rest of first line and discard
+  std::string rest_of_line;
+  std::getline(is, rest_of_line);
+
+  // second line = comment
+  std::string comment;
+  std::getline(is, comment);
+
+  // rest of lines are atoms (and unit cell parameters, if pbc = true)
+  const auto nlines_expected = natom + (pbc ? 3 : 0);
+  std::vector<Atom> atoms(natom, Atom{0, 0.0, 0.0, 0.0});
+  std::array<std::array<double, 3>, 3> unit_cell({{0.0, 0.0, 0.0}});
+  for (auto line = 0, atom_index = 0; line < nlines_expected; ++line) {
+    if (is.eof())
+      throw std::runtime_error(std::string("libint2::read_dotxyz: expected ") +
+                               std::to_string(nlines_expected) +
+                               " sets of coordinates but only " +
+                               std::to_string(line) + " received");
+
+    // read line
+    std::string linestr;
+    std::getline(is, linestr);
+    std::istringstream iss(linestr);
+    // then parse ... this handles "extended" XYZ formats
+    std::string element_symbol;
+    double x, y, z;
+    iss >> element_symbol >> x >> y >> z;
+
+    // .xyz files report Cartesian coordinates in angstroms; convert to bohr
+    const auto angstrom_to_bohr = 1 / bohr_to_angstrom;
+
+    auto assign_atom = [angstrom_to_bohr](Atom &atom, int Z, double x, double y,
+                                          double z) {
+      atom.atomic_number = Z;
+      atom.x = x * angstrom_to_bohr;
+      atom.y = y * angstrom_to_bohr;
+      atom.z = z * angstrom_to_bohr;
+    };
+    auto assign_xyz = [angstrom_to_bohr](std::array<double, 3> &xyz, double x,
+                                         double y, double z) {
+      xyz[0] = x * angstrom_to_bohr;
+      xyz[1] = y * angstrom_to_bohr;
+      xyz[2] = z * angstrom_to_bohr;
+    };
+
+    auto axis = -1;
+    // if pbc = true, look for unit cell params
+    if (pbc) {
+      if (strcaseequal("AA", element_symbol))
+        axis = 0;
+      if (strcaseequal("BB", element_symbol))
+        axis = 1;
+      if (strcaseequal("CC", element_symbol))
+        axis = 2;
+      if (axis != -1) {
+        assign_xyz(unit_cell[axis], x, y, z);
+      }
+    }
+
+    // .xyz files report element labels, hence convert to atomic numbers
+    if (axis == -1) {
       int Z = -1;
-      for(const auto& e: libint2::chemistry::get_element_info()) {
+      using libint2::chemistry::element_info;
+      for (const auto &e : element_info) {
         if (strcaseequal(e.symbol, element_symbol)) {
           Z = e.Z;
           break;
         }
       }
       if (Z == -1) {
-        std::ostringstream oss;
-        oss << "read_dotxyz: element symbol \"" << element_symbol << "\" is not recognized" << std::endl;
-        throw std::logic_error(oss.str().c_str());
+        std::cerr << "libint2::read_dotxyz: element symbol \"" << element_symbol
+                  << "\" is not recognized" << std::endl;
+        throw "Did not recognize element symbol in .xyz file";
       }
 
-      atoms[i].atomic_number = Z;
-
-      // .xyz files report Cartesian coordinates in angstroms; convert to bohr
-      atoms[i].x = x * angstrom_to_bohr;
-      atoms[i].y = y * angstrom_to_bohr;
-      atoms[i].z = z * angstrom_to_bohr;
+      assign_atom(atoms[atom_index++], Z, x, y, z);
     }
-
-    return atoms;
   }
 
-  /// converts a vector of <code>Atom</code>s to a vector of point charges
-  std::vector<std::pair<
-      double,
-      std::array<double, 3>>> inline make_point_charges(const std::
-                                                            vector<
-                                                                libint2::Atom>&
-                                                                atoms) {
-    std::vector<std::pair<double, std::array<double, 3>>> q;
-    q.reserve(atoms.size());
-    for (const auto& atom : atoms) {
-      q.emplace_back(static_cast<double>(atom.atomic_number),
-                     std::array<double, 3>{{atom.x, atom.y, atom.z}});
-    }
-    return q;
+  return std::make_tuple(atoms, unit_cell);
+}
+
+}  // anonymous namespace
+
+namespace libint2 {
+
+/// reads the list of atoms from a file in the standard XYZ format supported
+/// by most chemistry software (see <a
+/// href="https://en.wikipedia.org/wiki/XYZ_file_format">the Wikipedia
+/// page</a>)
+/// \param is[in] the std::istream object from which the data will be read
+/// \param bohr_to_angstrom[in] the conversion factor from Bohr (atomic unit
+/// of length; Libint uses atomic units throughout) to angstrom (in which
+/// the Cartesian coordinates are given in the XYZ file). The default is
+/// the 2010 CODATA value given by the
+/// libint2::constants::codata_2010::bohr_to_angstrom
+/// constant.
+/// \return a std::vector of Atom objects
+/// \throw std::logic_error if cannot parse the contents of \c is
+inline std::vector<Atom> read_dotxyz(
+    std::istream &is,
+    const double bohr_to_angstrom = constants::codata_2010::bohr_to_angstrom) {
+  std::vector<Atom> atoms;
+  std::tie(atoms, std::ignore) =
+      __libint2_read_dotxyz(is, bohr_to_angstrom, false);
+  return atoms;
+}
+
+/// reads the list of atoms from a file in the PBC-extended XYZ format
+/// \note The unit cell vectors in PBC-extended XYZ file are specified as atoms with
+///       element symbols "AA", "BB", and "CC" (N.B. the element symbols are not
+///       case-sensitive). If a unit cell vector is omitted, it is assumed to be zero. Omitting all three unit cell vectors is equivalent to an infinite unit cell (no periodicity in any direction).
+/// \param is[in] the std::istream object from which the data will be read
+/// \param bohr_to_angstrom[in] the conversion factor from Bohr (atomic unit
+/// of length; Libint uses atomic units throughout) to angstrom (in which
+/// the Cartesian coordinates are given in the XYZ file). The default is
+/// the 2010 CODATA value given by the
+/// libint2::constants::codata_2010::bohr_to_angstrom
+/// constant.
+/// \return a std::vector of Atom objects
+/// \throw std::logic_error if cannot parse the contents of \c is
+/// \return a tuple composed of the list of atoms and an array of 3
+///         unit cell vectors, \c A , \c B , and \c C .
+inline auto read_dotxyz_pbc(
+    std::istream &is,
+    const double bohr_to_angstrom = constants::codata_2010::bohr_to_angstrom)
+    -> decltype(__libint2_read_dotxyz(is, bohr_to_angstrom, true)) {
+  return __libint2_read_dotxyz(is, bohr_to_angstrom, true);
+}
+
+/// converts a vector of <code>Atom</code>s to a vector of point charges
+std::vector<std::pair<double, std::array<double, 3>>> inline make_point_charges(
+    const std::vector<libint2::Atom> &atoms) {
+  std::vector<std::pair<double, std::array<double, 3>>> q(atoms.size());
+  for (const auto &atom : atoms) {
+    q.emplace_back(static_cast<double>(atom.atomic_number),
+                   std::array<double, 3>{{atom.x, atom.y, atom.z}});
   }
+  return q;
+}
 
 } // namespace libint2
 
