182c60e90d948090e5f68257b8de28eaa39390a2
Eduard Valeyev <eduard@valeyev.net>
2021-10-07 08:04:53 -0400

    amended 59db8d447ceb2cecba25c4326797c74d0762f323
    (cherry picked from commit e022cc0b177f70e54b53168603e133f9781b7b4d)

--- include/libint2/cxxapi.h
+++ include/libint2/cxxapi.h
@@ -32,6 +32,10 @@
 # error "C++11 API does not support with user-defined real types yet; omit --with-real-type when configuring"
 #endif
 
+#if !defined(INCLUDE_ONEBODY) || !(defined(INCLUDE_ERI) || defined(INCLUDE_ERI3) || defined(INCLUDE_ERI2))
+# error "C++ API is only supported if both 1-body and some (eri, eri3, eri2) 2-body integrals are enabled"
+#endif
+
 #include <libint2/initialize.h>
 #include <libint2/chemistry/elements.h>
 #include <libint2/atom.h>
