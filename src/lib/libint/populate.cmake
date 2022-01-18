69d48fb377c3bc0b30a9d3860af2533b74f3d87e
Lori A. Burns <lori.burns@gmail.com>
2022-01-18 13:29:51 -0500

    move eigen detection to config

--- src/lib/libint/populate.cmake
+++ src/lib/libint/populate.cmake
@@ -52,6 +52,7 @@ file(INSTALL "${PROJECT_SOURCE_DIR}/cmake/modules/autocmake_safeguards.cmake"
         "${PROJECT_SOURCE_DIR}/cmake/modules/options.cmake"
         "${PROJECT_SOURCE_DIR}/cmake/modules/FindMPFR.cmake"
         "${PROJECT_SOURCE_DIR}/cmake/modules/AddCustomTargetSubproject.cmake"
+        "${PROJECT_SOURCE_DIR}/cmake/modules/FindTargetEigen3.cmake"
         "${PROJECT_BINARY_DIR}/cmake/modules/int_computed.cmake"
         DESTINATION "${EXPORT_STAGE_DIR}/cmake/modules")
 
