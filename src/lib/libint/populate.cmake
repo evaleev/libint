1574ad2dd281e2272e87d08bae24c613f3900dd4
Lori A. Burns <lori.burns@gmail.com>
2022-01-21 17:32:27 -0500

    help pkgconfig file paths

--- src/lib/libint/populate.cmake
+++ src/lib/libint/populate.cmake
@@ -53,6 +53,7 @@ file(INSTALL "${PROJECT_SOURCE_DIR}/cmake/modules/autocmake_safeguards.cmake"
         "${PROJECT_SOURCE_DIR}/cmake/modules/FindMPFR.cmake"
         "${PROJECT_SOURCE_DIR}/cmake/modules/AddCustomTargetSubproject.cmake"
         "${PROJECT_SOURCE_DIR}/cmake/modules/FindTargetEigen3.cmake"
+        "${PROJECT_SOURCE_DIR}/cmake/modules/JoinPaths.cmake"
         "${PROJECT_BINARY_DIR}/cmake/modules/int_computed.cmake"
         DESTINATION "${EXPORT_STAGE_DIR}/cmake/modules")
 
