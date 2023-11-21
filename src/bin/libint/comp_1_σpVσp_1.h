2ad4ca132359341f06cb04fcd98d51781e3d9a67
Jonathon Misiewicz <jonathon.paul.misiewicz@emory.edu>
2023-11-20 20:39:30 -0600

    Initial implmentation of addl. W terms. Still need to test

--- src/bin/libint/comp_1_σpVσp_1.h
+++ src/bin/libint/comp_1_σpVσp_1.h
@@ -93,11 +93,65 @@ CR_1_σpVσp_1<F>::CR_1_σpVσp_1(const SafePtr< TargetType >& Tint,
     auto Dz_a_V_Dz_b = factory.make_child(Dz_a, Dz_b, zero_m);
     if (is_simple()) {
       expr_ = Scalar(-1) *
-              (Dx_a_V_Dx_b + Dy_a_V_Dy_b + Dz_a_V_Dz_b); // smth like this
+              (Dx_a_V_Dx_b + Dy_a_V_Dy_b + Dz_a_V_Dz_b);
       nflops_ += 3;
     }
   } break;
-
+  // N.B.: Wz is needed after W0 because it's needed for 2-component methods. Wx and Wy are not.
+  // (a|Wz|b) = (d a/dAx | V | d b/dBy) - (d a/dAy | V | d b/dBx)
+  case 1: {
+    F Dx_a{a};
+    Dx_a.deriv().inc(x);
+    F Dy_b{b};
+    Dy_b.deriv().inc(y);
+    auto Dx_a_V_Dy_b = factory.make_child(Dx_a, Dy_b, zero_m);
+    F Dy_a{a};
+    Dy_a.deriv().inc(y);
+    F Dx_b{b};
+    Dx_b.deriv().inc(x);
+    auto Dy_a_V_Dx_b = factory.make_child(Dy_a, Dx_b, zero_m);
+    if (is_simple()) {
+      expr_ = Scalar(-1) *
+              (Dx_a_V_Dy_b - Dy_a_V_Dx_b);
+      nflops_ += 2;
+    }
+  } break;
+  // (a|Wx|b) = (d a/dAy | V | d b/dBz) - (d a/dAz | V | d b/dBy)
+  case 2: {
+    F Dy_a{a};
+    Dy_a.deriv().inc(y);
+    F Dz_b{b};
+    Dz_b.deriv().inc(z);
+    auto Dy_a_V_Dz_b = factory.make_child(Dy_a, Dz_b, zero_m);
+    F Dz_a{a};
+    Dz_a.deriv().inc(z);
+    F Dy_b{b};
+    Dy_b.deriv().inc(y);
+    auto Dz_a_V_Dy_b = factory.make_child(Dz_a, Dy_b, zero_m);
+    if (is_simple()) {
+      expr_ = Scalar(-1) *
+              (Dy_a_V_Dz_b - Dz_a_V_Dy_b);
+      nflops_ += 2;
+    }
+  } break;
+  // (a|Wy|b) = (d a/dAz | V | d b/dBx) - (d a/dAx | V | d b/dBz)
+  case 3: {
+    F Dz_a{a};
+    Dz_a.deriv().inc(z);
+    F Dx_b{b};
+    Dx_b.deriv().inc(x);
+    auto Dz_a_V_Dx_b = factory.make_child(Dz_a, Dx_b, zero_m);
+    F Dx_a{a};
+    Dx_a.deriv().inc(x);
+    F Dz_b{b};
+    Dz_b.deriv().inc(z);
+    auto Dx_a_V_Dz_b = factory.make_child(Dx_a, Dz_b, zero_m);
+    if (is_simple()) {
+      expr_ = Scalar(-1) *
+              (Dz_a_V_Dx_b - Dx_a_V_Dz_b);
+      nflops_ += 2;
+    }
+  } break;
   default:
     throw std::runtime_error("CR_1_σpVσp_1: invalid Pauli index");
   }
