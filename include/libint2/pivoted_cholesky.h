cb046478095ee97a7e1bf0d2eb516f3b6f74345f
Kshitij Surjuse <kshitijsurjuse100@gmail.com>
2023-11-23 18:02:23 -0500

    reformatted code in `bfbs_generator.h` and `pivoted_cholesky.h`

--- include/libint2/pivoted_cholesky.h
+++ include/libint2/pivoted_cholesky.h
@@ -1,24 +1,41 @@
-//
-// Created by Kshitij Surjuse on 11/15/23.
-//
+/*
+ *  Copyright (C) 2004-2021 Edward F. Valeev
+ *
+ *  This file is part of Libint.
+ *
+ *  Libint is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Libint is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Libint.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-#ifndef LIBINT_PIVOTED_CHOLESKY_H
-#define LIBINT_PIVOTED_CHOLESKY_H
+#ifndef _libint2_src_lib_libint_pivoted_cholesky_h_
+#define _libint2_src_lib_libint_pivoted_cholesky_h_
 
-
-#include <iostream>
 #include <Eigen/Dense>
 #include <algorithm>
+#include <iostream>
 
 namespace libint2 {
 
-    /// @brief computes the pivoted Cholesky decomposition of a symmetric positive definite matrix
-    /// @param A symmetric positive definite matrix
-    /// @param tolerance tolerance for the error
-    /// @param pivot initial pivot indices
-    /// @return pivoted Cholesky decomposition of A
-    inline std::vector<size_t>
-    pivoted_cholesky(const Eigen::MatrixXd &A, const double &tolerance, const std::vector<size_t> &pivot) {
+/// @brief computes the pivoted Cholesky decomposition of a symmetric positive
+/// definite matrix
+/// @param A symmetric positive definite matrix
+/// @param tolerance tolerance for the error
+/// @param pivot initial pivot indices
+/// @return pivoted Cholesky decomposition of A
+    inline std::vector<size_t> pivoted_cholesky(const Eigen::MatrixXd &A,
+                                                const double tolerance,
+                                                const std::vector<size_t> &pivot) {
         // number of elements in A
         auto n = A.rows();
         // diagonal elements of A
@@ -44,7 +61,6 @@ namespace libint2 {
         }
 
         while (error > tolerance && m < n) {
-
             // update pivot indices
             // Errors in pivoted order
             std::vector<double> err(d.size());
@@ -58,7 +74,8 @@ namespace libint2 {
                 idx[i] = i;
             }
             // sort indices
-            std::sort(idx.begin(), idx.end(), [&err2](size_t i1, size_t i2) { return err2[i1] > err2[i2]; });
+            std::sort(idx.begin(), idx.end(),
+                      [&err2](size_t i1, size_t i2) { return err2[i1] > err2[i2]; });
             // subvector of piv
             std::vector<size_t> piv_subvec(piv.size() - m);
             for (size_t i = 0; i < piv_subvec.size(); ++i) {
@@ -76,26 +93,28 @@ namespace libint2 {
             // compute diagonal element
             L(m, pim) = std::sqrt(d[pim]);
 
-            //off-diagonal elements
+            // off-diagonal elements
             for (size_t i = m + 1; i < n; ++i) {
                 auto pii = piv[i];
-                //compute element
-                L(m, pii) = (m > 0) ? (A(pim, pii) - L.col(pim).head(m).dot(L.col(pii).head(m))) / L(m, pim) :
-                            (A(pim, pii)) / L(m, pim);
-                //update d
+                // compute element
+                L(m, pii) =
+                        (m > 0) ? (A(pim, pii) - L.col(pim).head(m).dot(L.col(pii).head(m))) /
+                                  L(m, pim)
+                                : (A(pim, pii)) / L(m, pim);
+                // update d
                 d[pii] -= L(m, pii) * L(m, pii);
             }
-            //update error
+            // update error
             if (m + 1 < n) {
                 error = d[piv[m + 1]];
                 for (size_t i = m + 1; i < n; ++i) {
                     error = std::max(d[piv[i]], error);
                 }
             }
-            //increase m
+            // increase m
             m++;
         }
-        //Transpose to get Cholesky vectors as columns
+        // Transpose to get Cholesky vectors as columns
         L.transposeInPlace();
         // Drop unnecessary columns
         L.conservativeResize(n, m);
@@ -110,6 +129,6 @@ namespace libint2 {
         return reduced_piv;
     }
 
-}
+}  // namespace libint2
 
-#endif //LIBINT_PIVOTED_CHOLESKY_H
+#endif  //_libint2_src_lib_libint_pivoted_cholesky_h_
